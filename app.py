a0dfce9731d2a1e06d1586e86da6e6bffe2f9bf9
Aabirah <aabirah04@gmail.com>
2026-02-13 15:33:53 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import numpy as np
+import joblib
+
+# Load model
+model = joblib.load("iris_model.pkl")
+
+st.title("🌸 Iris Flower Classification App")
+
+st.write("Enter flower measurements to predict species")
+
+# Input fields
+sepal_length = st.number_input("Sepal Length")
+sepal_width = st.number_input("Sepal Width")
+petal_length = st.number_input("Petal Length")
+petal_width = st.number_input("Petal Width")
+
+if st.button("Predict"):
+    features = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
+    prediction = model.predict(features)
+    
+    species = ["Setosa", "Versicolor", "Virginica"]
+    
+    st.success(f"Predicted Species: {species[prediction[0]]}")
